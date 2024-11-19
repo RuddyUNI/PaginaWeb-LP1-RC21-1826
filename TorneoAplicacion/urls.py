5ee3f6b26636f425a28f224d50d91dd9c3765439
RuddyUNI <rc21-1826@unphu.edu.do>
2024-11-19 18:47:50 -0400

    TemplatesTerminado

--- TorneoAplicacion/urls.py
+++ TorneoAplicacion/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Productos, name='Productos'),
-    path('Principal/', views.index, name='index'),
+    path('holamundo/', views.Productos, name='Productos'),
+    path('', views.index, name='index'),
 ]
