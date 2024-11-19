6119cd0ab742508b4b7fb247a71f033b26e0aae6
RuddyUNI <rc21-1826@unphu.edu.do>
2024-11-19 18:26:07 -0400

    Prueba

---
+++ TorneoAplicacion/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.urls import path
+
+# Create your views here.
+
+def Productos(request):
+    return HttpResponse("<h1>Nuestra primera vista!</h1>")
+
