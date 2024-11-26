e18bfd0a15d197ca04710032a4837b58725792b7
RuddyUNI <rc21-1826@unphu.edu.do>
2024-11-26 18:32:08 -0400

    ImagenesArregladas en index.html

---
+++ TorneoAplicacion/static/js/index.js
@@ -0,0 +1,7 @@
+// Alternancia del modo oscuro
+const darkModeToggle = document.getElementById('darkModeToggle');
+const body = document.body;
+
+darkModeToggle.addEventListener('click', () => {
+  body.classList.toggle('dark-mode');
+});
