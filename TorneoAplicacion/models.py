fbcb9309cbb44152d84bb77ff80d10cbd7740539
RuddyUNI <rc21-1826@unphu.edu.do>
2024-11-26 18:08:59 -0400

    PruebaConDisponibleoNoDisponible

--- TorneoAplicacion/models.py
+++ TorneoAplicacion/models.py
@@ -5,3 +5,6 @@ class productos(models.Model):
     codigoProducto = models.IntegerField()
     descripcionProducto = models.CharField(max_length=255)
     estatus = models.BooleanField()
+
+    def estatus_display(self):
+        return "Disponible" if self.estatus else "No disponible"
